1e11355f08eac47afd60efaf39ad32992db6d2f5
Karoline Busse <kikidergecko@hannover.ccc.de>
2013-04-26 21:51:54 +0200

    humidity sensor with voltage flipping
    schematic update coming soon.

--- plant_guard_arduino/plant_guard_arduino.ino
+++ plant_guard_arduino/plant_guard_arduino.ino
@@ -1,21 +1,44 @@
 
 #define TEMP_IN A0
 #define HUMID_IN A1
+#define VOLTAGE_FLIP_1 2
+#define VOLTAGE_FLIP_2 3
+
+#define FLIP_TIMER 1000
 
 int out = 0;
 int temp_value = 0;
-int humid_value = 0;
+int humid_value_1 = 0;
+int humid_value_2 = 0;
+int humid_result = 0;
 
 void setup() {
   Serial.begin(115200);
-  //pinMode(HUMID_OUT, OUTPUT);
-  //pinMode(HUMID_IN, INPUT);
-  //digitalWrite(HUMID_OUT, out);
+  pinMode(VOLTAGE_FLIP_1, OUTPUT);
+  pinMode(VOLTAGE_FLIP_2, OUTPUT);
+  pinMode(HUMID_IN, INPUT);
+}
+
+void setSensorPolarity(boolean flip){
+  if(flip){
+    digitalWrite(VOLTAGE_FLIP_1, HIGH);
+    digitalWrite(VOLTAGE_FLIP_2, LOW);
+  } else {
+    digitalWrite(VOLTAGE_FLIP_1, LOW);
+    digitalWrite(VOLTAGE_FLIP_2, HIGH);
+  }
 }
 
 void loop(){
   // humidity sensing
-  humid_value = analogRead(HUMID_IN);
+  setSensorPolarity(true);
+  delay(FLIP_TIMER);  
+  humid_value_1 = analogRead(HUMID_IN);
+  delay(FLIP_TIMER);
+  setSensorPolarity(false);
+  delay(FLIP_TIMER);
+  humid_value_2 = 1023 - analogRead(HUMID_IN);
+  humid_result = (humid_value_1 + humid_value_2) / 2;
   
   // temperature sensing
   temp_value = analogRead(TEMP_IN);
@@ -24,6 +47,6 @@ void loop(){
   
   // output
   //Serial.println(voltage);
-  Serial.println(humid_value);
-  delay(1000);
+  Serial.println(humid_result);
+  //delay(1000);
 }
