d3defe0dc2066394807059e2261f26e461c2389d
Nicola <jasinai@jasinai.de>
2013-07-11 14:07:33 +0200

    Improved EEPROM management via push button Added ONLINE flag to enable/disable WiFly Button now disconnected from waternig function

--- plant_guard_arduino/plant_guard_arduino.ino
+++ plant_guard_arduino/plant_guard_arduino.ino
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <WiFlyHQ.h>
 #include <EEPROM.h>
+#include <avr/sleep.h>
 #include "config.h"
 #include "WiFlyTwitter.h"
 
@@ -10,14 +11,26 @@
 #define VOLTAGE_FLIP_1 4
 #define VOLTAGE_FLIP_2 5
 #define WATER_VALVE 7
-#define WATER_BUTTON 2
+#define BUTTON_PIN 2
 #define FLIP_TIMER 1000
 #define ONBOARD_LED 13
+#define EEPROM_COUNTER_ADDRESS 1022
+
+// Arbitrary constants for thresholds, adjust as necessary
+#define DRY 100
+#define WET 900
+#define DELTA_HUMID 50
+#define TOO_COLD 5
+#define TOO_HOT 30
+
 
 //#define MEASURE_INTERVAL (10) // in seconds
-#define MEASURE_INTERVAL (60*1) // in seconds
+#define MEASURE_INTERVAL (60*10) // in seconds
+
+#define ONLINE false
 
 short watered = 0;
+uint8_t button_pressed = 0;
 int out = 0;
 int temp_value = 0;
 uint8_t temperature = 0;
@@ -43,50 +56,29 @@ void setup() {
   OCR1A = 15625;
   // Enable Timer1 output compare A interrupt
   TIMSK1 |= (1 << OCIE1A);
-  pinMode(ONBOARD_LED, OUTPUT);
 
   Serial.begin(115200);
+  pinMode(ONBOARD_LED, OUTPUT);
   pinMode(VOLTAGE_FLIP_1, OUTPUT);
   pinMode(VOLTAGE_FLIP_2, OUTPUT);
   pinMode(HUMID_IN, INPUT);
   pinMode(WATER_VALVE, OUTPUT);
   digitalWrite(WATER_VALVE, LOW);
-  twitter.setupWiFly();
-  pinMode(WATER_BUTTON, INPUT_PULLUP);
+
+  // Button for accessing the data
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   delay(10);
   EIMSK |= (1 << INT0);     // Enable external interrupt INT0
   EICRA |= (1 << ISC01);    // Trigger INT0 on falling edge
   EIFR = (1 << INTF0);      // Reset Interrupt flag
   sei(); // library function for interrrupt enabling
-  //attachInterrupt(0, waterButton, FALLING);
-  //twitter.post("I'm alive");
 
-  // Sleep, mode = "idle" (000) und Sleep Enable = 1
-  SMCR = 1;
+  if (ONLINE) twitter.setupWiFly();
+  //if (ONLINE) twitter.post("I'm alive");
 
-  // Reset values that are stored in EEPROM by setting the value counter to 0.
-  if (false)
-  {
-    EEPROM.write(1022, 0);
-    EEPROM.write(1023, 0);
-  }
+  set_sleep_mode(SLEEP_MODE_IDLE);
 
-  // Number of values is stored as 16 bit (little endian) value in EEPROM
-  eeprom_count = (EEPROM.read(1022)) | (EEPROM.read(1023) << 8);
-
-  // Read out all values stored in EEPROM
-  if (true) {
-    delay(1000);
-    Serial.print("EEPROM contains ");
-    Serial.print(eeprom_count);
-    Serial.println(" values:");
-    for (uint16_t i = 0; i < eeprom_count; i++) {
-      Serial.print(EEPROM.read(i));
-      Serial.print(" ");
-    }
-    Serial.println();
-  }
-
+  // Take a measurement as soon the loop starts
   next_measurement = seconds;
 }
 
@@ -122,21 +114,42 @@ float measure_temperature()
     return (uint8_t) (temp_value * 5.0 * 10.0) / 1023.0;
 }
 
+void read_out_eeprom() {
+  uint16_t count = eeprom_read_uint16(EEPROM_COUNTER_ADDRESS);
+  Serial.print("EEPROM contains ");
+  Serial.print(count);
+  Serial.println(" values:");
+  for (uint16_t i = 0; i < count; i++) {
+    Serial.println(EEPROM.read(i));
+  }
+}
 
-ISR(INT0_vect) // WATER_BUTTON is low (trigger on low because it was set to HIGH before)
+void eeprom_write_uint16(uint16_t address, uint16_t value) {
+  EEPROM.write(address, value & 0xFF); // Low byte
+  EEPROM.write(address + 1, (value >> 8) & 0xFF); // High byte
+}
+
+uint16_t eeprom_read_uint16(uint16_t address) {
+  return (EEPROM.read(address)) | (EEPROM.read(address + 1) << 8);
+}
+
+
+ISR(INT0_vect) // BUTTON_PIN is low (trigger on low because it was set to HIGH before)
 {
-  watered++;
+  button_pressed = 1;
 }
 
 // Timer1 compare match interrupt. Will be called every second.
 ISR(TIMER1_COMPA_vect)
 {
   seconds++;
-  digitalWrite(ONBOARD_LED, seconds & 1 ? HIGH : LOW);
 }
 
 void loop()
 {
+  // Toggle LED every second so that we know the program is not stuck somewhere
+  digitalWrite(ONBOARD_LED, seconds & 1 ? HIGH : LOW);
+
   if (seconds >= next_measurement) {
     next_measurement += MEASURE_INTERVAL;
     Serial.print("Measuring... ");
@@ -147,7 +160,7 @@ void loop()
       digitalWrite(WATER_VALVE, HIGH);
       delay(3000);
       digitalWrite(WATER_VALVE, LOW);
-      watered++;
+      //watered++;
     }
     
     if(last_humidity > humid_result && last_humidity - humid_result > DELTA_HUMID){ // plant was most likely watered
@@ -156,33 +169,35 @@ void loop()
     }
     
     temperature = measure_temperature();
-    string msg[141];
+    char msg[141];
     if(temperature <= TOO_COLD){
       snprintf(msg, 141, "Mir ist kalt! Stell mich an einen wärmeren Ort. Aktuelle Temperatur: %u", temperature);
-      Serial.println(twitter.post(msg));
+      msg[140] = '\0';  // snprintf doesn't write \0 if string is too long.
+      if (ONLINE) Serial.println(twitter.post(msg));
     }
     else if(temperature >= TOO_HOT){
       snprintf(msg, 141, "Puh, ist das warm! Ein bisschen Schatten wäre nicht schlecht. Aktuelle Temperatur: %u", temperature);
-      Serial.println(msg);
+      msg[140] = '\0';  // snprintf doesn't write \0 if string is too long.
+      if (ONLINE) Serial.println(twitter.post(msg));
     }
     Serial.println(humid_result);
     // output
     //Serial.println(voltage);
 
-    // Scale 10 bit value to 8 bits, save in EEPROM and increase counter.
-    if (eeprom_count < 1022)
+    // Store humidity values (scaled to 8 bit) in EEPROM
+    eeprom_count = eeprom_read_uint16(EEPROM_COUNTER_ADDRESS);
+    if (eeprom_count < EEPROM_COUNTER_ADDRESS)
     {
-      EEPROM.write(eeprom_count, (uint8_t) (humid_result / 4));
+      EEPROM.write(eeprom_count, (uint8_t) (humid_result >> 2));
       eeprom_count++;
-      EEPROM.write(1022, eeprom_count & 0xFF); // Low byte
-      EEPROM.write(1023, (eeprom_count >> 8) & 0xFF); // High byte
+      eeprom_write_uint16(EEPROM_COUNTER_ADDRESS, eeprom_count);
     }
 
     sprintf(s+strlen(s), "%d ", humid_result);
     count++;
     if(count == 24)
     {
-      Serial.println(twitter.post(s));
+      if (ONLINE) Serial.println(twitter.post(s));
       count = 0;
       *s = '\0';
     }
@@ -194,14 +209,33 @@ void loop()
 
   if(watered)
   {
-    Serial.print("Watered! Tweeting... ");
-    Serial.println(twitter.post("Test"));
+    Serial.println("Watered! Tweeting...");
+    if (ONLINE) Serial.println(twitter.post("Test"));
     //Serial.println(watered);
     watered = 0;
+  }
+
+  if (button_pressed)
+  {
+    button_pressed = 0;
+    read_out_eeprom();
+
+    uint8_t i = 0, steps = 200;
+    for (i = 0; i < steps; i++)
+    {
+      if (digitalRead(BUTTON_PIN) == HIGH) break; // Escape when button is released.
+      delay(10);
+    }
+    if (i == steps) // The button was pressed during the whole loop.
+    {
+      Serial.println("Button was held for more than 2 seconds, will now erase EEPROM.");
+      eeprom_write_uint16(EEPROM_COUNTER_ADDRESS, 0);
+    }
   }
 
   // Power down the microcontroller.
   // Will wake up every second due to Timer1 compare match.
   //asm("sleep\n");
-  delay(100);
+  //delay(100);
+  sleep_mode();
 }
